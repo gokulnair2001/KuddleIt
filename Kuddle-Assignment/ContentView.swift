3469ccc3cd7c2706d1029907cc7f707fd0e5be7e
Gokul Nair <gokulnair.2001@gmail.com>
2022-08-02 20:09:16 +0530

    Initial Commit

---
+++ Kuddle-Assignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kuddle-Assignment
+//
+//  Created by Gokul Nair on 02/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kuddle-Assignment/Kuddle_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kuddle_AssignmentApp.swift
+//  Kuddle-Assignment
+//
+//  Created by Gokul Nair on 02/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Kuddle_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
